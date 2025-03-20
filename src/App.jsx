8c8511afc1654517728d3748d4aaee3220cc8477
Hans Bregman <hns101@live.nl>
2025-03-20 18:56:40 +0100

    added setup and fetching pokeapi

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,46 @@
 import './App.css'
+import {useEffect, useState} from "react";
+import axios from "axios";
+import pokeApiLogo from "./assets/pokeapi_256.png";
 
 function App() {
 
-  return (
-    <>
-      <h1>Gotta catch em all!</h1>
-    </>
-  )
+    const API_URI = 'https://pokeapi.co/api/v2/pokemon/charmander';
+    const [data, setData] = useState([]);
+
+
+    const fetchData = async () => {
+        try {
+            const response = await axios.get(API_URI);
+            setData(response.data);
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+
+        useEffect(() => {
+            fetchData();
+        }, [])
+
+        return (
+            <>
+                <h1>Pokedex</h1>
+                <img src={pokeApiLogo} className="poke-api-logo" alt="Poke API Logo"/>
+                <main className="main">
+
+                    <div className="poke-card">
+                        <p>{data.name}</p>
+                        <p>Weigth {data.weight}</p>
+                        <p>{data.moves?.length || 0}</p>
+                    </div>
+
+                </main>
+
+
+            </>
+        )
 }
 
 export default App
